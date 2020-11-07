6d23f0aea864e94b02508b5f26e8aa83fa93ab06
Tom Shen <tomshen@berkeley.edu>
2020-11-06 16:27:10 -0800

    verifier beta

--- src/lib.rs
+++ src/lib.rs
@@ -13,12 +13,15 @@ pub mod ahp;
 
 pub use error::Error;
 use ark_relations::r1cs::Matrix;
-use crate::ahp::indexer::IndexPK;
+use crate::ahp::indexer::{IndexPK, IndexVK};
 use crate::ahp::AHPForSpartan;
-use crate::error::SResult;
+use crate::error::{SResult, invalid_arg};
 use crate::data_structures::proof::Proof;
 use linear_sumcheck::data_structures::Blake2s512Rng;
 use linear_sumcheck::data_structures::random::FeedableRNG;
+use ark_std::collections::LinkedList;
+use ark_std::iter::FromIterator;
+use linear_sumcheck::ml_sumcheck::ahp::prover::ProverMsg;
 
 /// module for interpret r1cs as ML Extension used by linear sumcheck
 pub mod data_structures;
@@ -114,9 +117,77 @@ impl<F: Field> Spartan<F> {
             prover_sixth_message: pm6
         })
 
-
     }
-    // pub fn
+    pub fn verify(vk: IndexVK<F>, proof: Proof<F>) -> SResult<bool> {
+
+        let log_n = vk.log_n;
+
+        let mut first_sumcheck_messages = LinkedList::from_iter(proof.first_sumcheck_messages.into_iter());
+        let mut second_sumcheck_messages = LinkedList::from_iter(proof.second_sumcheck_messages.into_iter());
+
+
+        let mut fs_rng = Blake2s512Rng::setup();
+        fs_rng.feed_randomness(&vk.matrix_a)?;
+        fs_rng.feed_randomness(&vk.matrix_b)?;
+        fs_rng.feed_randomness(&vk.matrix_c)?;
+        fs_rng.feed_randomness(&vk.v)?;
+
+        let vs = AHPForSpartan::verifier_init(vk);
+
+        let pm = proof.prover_first_message;
+        fs_rng.feed_randomness(&pm)?;
+        let (vs, _) = AHPForSpartan::verify_first_round(vs, pm, &mut fs_rng)?;
+
+        let pm = proof.prover_second_message;
+        fs_rng.feed_randomness(&pm)?;
+        let (vs, _) = AHPForSpartan::verify_second_round(vs, pm, &mut fs_rng)?;
+
+        let pm = proof.prover_third_message;
+        fs_rng.feed_randomness(&pm)?;
+        let (mut vs, _) = AHPForSpartan::verify_third_round(vs, pm)?;
+
+        for i in 0..(log_n - 1) {
+            let pm = Self::try_pop(&mut first_sumcheck_messages)?;
+            fs_rng.feed_randomness(&pm)?;
+            let (vs_new, _) = AHPForSpartan::verify_first_sumcheck_ongoing_round(vs, pm, &mut rng)?;
+            vs = vs_new;
+        }
+
+        let pm = Self::try_pop(&mut first_sumcheck_messages)?;
+        fs_rng.feed_randomness(&pm)?;
+        let (vs, _) = AHPForSpartan::verify_first_sumcheck_final_round(vs, pm, &mut fs_rng)?;
+
+        let pm = proof.prover_fourth_message;
+        fs_rng.feed_randomness(&pm)?;
+        let (vs, _) = AHPForSpartan::verify_fourth_round(vs, pm, &mut fs_rng)?;
+
+        let pm = proof.prover_fifth_message;
+        fs_rng.feed_randomness(&pm)?;
+        let (mut vs, _) = AHPForSpartan::verify_fifth_round(vs, pm)?;
+
+        for _ in 0..(log_n - 1) {
+            let pm = Self::try_pop(&mut second_sumcheck_messages)?;
+            fs_rng.feed_randomness(&pm)?;
+            let (vs_new, _) = AHPForSpartan::verify_second_sumcheck_ongoing_round(vs, pm, &mut fs_rng)?;
+            vs = vs_new;
+        }
+
+        let pm = Self::try_pop(&mut second_sumcheck_messages)?;
+        fs_rng.feed_randomness(&pm)?;
+        let (vs, _) = AHPForSpartan::verify_second_sumcheck_final_round(vs, pm, &mut fs_rng)?;
+
+        let pm = proof.prover_sixth_message;
+        fs_rng.feed_randomness(&pm)?;
+
+        let result = AHPForSpartan::verify_sixth_round(vs, pm)?;
+
+        Ok(result)
+    }
+
+    fn try_pop(sumcheck_messages: &mut LinkedList<ProverMsg<F>>) -> SResult<ProverMsg<F>>{
+        sumcheck_messages.pop_front()
+            .ok_or(invalid_arg("malformed sumcheck message"))
+    }
 }
 
 
